ffdbb4cb35d2bd07837c10c89fd1db998b98cca4
Vladislav Zhgirov <zgv2324@gmail.com>
2023-03-04 21:12:08 +0300

    update app module

--- piskel-clone-app/src/app/app.module.ts
+++ piskel-clone-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ToolsModule} from '@shared/tools/tools.module';
+import {ToolsModule} from '@shared/tools-container/tools.module';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
