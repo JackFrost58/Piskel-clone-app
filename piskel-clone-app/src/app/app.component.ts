8ea09f78be31cf8b8f663445ae9ce2160a77c56a
Vladislav Zhgirov <zgv2324@gmail.com>
2023-03-04 21:57:37 +0300

    app: component binding

--- piskel-clone-app/src/app/app.component.ts
+++ piskel-clone-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {ConfigTool} from '@interfaces/config-tool.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public getConfigTool(item: any) {
-    console.log(item);
+  public configTool: ConfigTool;
+
+  public getConfigTool(item: ConfigTool) {
+    this.configTool = item;
   }
 }
