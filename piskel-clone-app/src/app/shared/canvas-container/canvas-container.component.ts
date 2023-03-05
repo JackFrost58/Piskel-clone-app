00f30feea34f44906f8a4d3f50ae18189afe8179
Vladislav Zhgirov <zgv2324@gmail.com>
2023-03-05 20:00:25 +0300

    replace js canvas to component

--- piskel-clone-app/src/app/shared/canvas-container/canvas-container.component.ts
+++ piskel-clone-app/src/app/shared/canvas-container/canvas-container.component.ts
@@ -1,15 +1,176 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {convertColor} from '@helpers/convert-colors.helper';
+import {getCoordinates} from '@helpers/coordinates.helper';
+import {ConfigTool} from '@interfaces/config-tool.interface';
+import {Point} from '@interfaces/coordinate.interface';
+import {NameTools} from '@shared/tools-container/entities/enums';
 
 @Component({
-  selector: 'app-canvas-container',
+  selector: 'canvas-container',
   templateUrl: './canvas-container.component.html',
   styleUrls: ['./canvas-container.component.scss']
 })
 export class CanvasContainerComponent implements OnInit {
+  @Input() toolConfig: ConfigTool;
 
-  constructor() { }
+  public startCoordinates: Point;
+  public endCoordinates: Point;
+  public context: CanvasRenderingContext2D | null;
+
+  public isDrawing = false;
+  
+  constructor() {}
 
   ngOnInit() {
+    this.startCoordinates = {x: 0, y: 0}
+    this.canvas.nativeElement.width = 32;
+    this.canvas.nativeElement.height = 32;
   }
 
+  @ViewChild('canvasElement', {static: true})
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  ngAfterViewInit(): void {
+    this.context = this.canvas.nativeElement.getContext('2d');
+  }
+
+  public useTool(startCoord: Point, currentCoord: Point, color: string, penSize: number): void {
+    if (this.context !== null) {    
+      const deltaX = Math.abs(currentCoord.x - startCoord.x);
+      const deltaY = Math.abs(currentCoord.y - startCoord.y);
+      const signX = (startCoord.x < currentCoord.x) ? 1 : -1;
+      const signY = (startCoord.y < currentCoord.y) ? 1 : -1;
+      let err = deltaX - deltaY;
+    
+      while(true) {
+        this.context.fillStyle = color;
+        this.context?.fillRect(startCoord.x, startCoord.y, penSize, penSize) 
+    
+        if ((startCoord.x === Number(currentCoord.x)) && (startCoord.y === Number(currentCoord.y))) break;
+        const e2 = 2 * err;
+        if (e2 > -deltaY) { err -= deltaY; startCoord.x  += signX; }
+        if (e2 < deltaX) { err += deltaX; startCoord.y  += signY; }
+      }
+    }
+  }
+
+  public canvasMouseDownHandler(e: MouseEvent): void {
+    if (this.toolConfig.colorValue && this.toolConfig.penSize) {
+      this.isDrawing = true;
+      this.startCoordinates = getCoordinates(this.canvas, e);
+      this.useTool(this.startCoordinates, this.startCoordinates, this.toolConfig.colorValue, this.toolConfig.penSize);
+    }
+  }
+
+  public canvasMouseMoveHandler(e: MouseEvent): void {
+    let currentCoordinates;
+    if (this.toolConfig.colorValue && this.toolConfig.penSize) {
+      switch (this.toolConfig.nameTool) {
+        case NameTools.Eraser:
+        case NameTools.Pen: 
+          currentCoordinates = getCoordinates(this.canvas, e);
+          this.startCoordinates = currentCoordinates;
+          if(!this.isDrawing) return;
+          this.useTool(this.startCoordinates, currentCoordinates, this.toolConfig.colorValue, this.toolConfig.penSize);
+          break;
+        case NameTools.Stroke:
+          currentCoordinates = getCoordinates(this.canvas, e);
+          this.endCoordinates = currentCoordinates;
+          break;
+      }
+    }
+  }
+
+  public canvasMouseUpHandler(e: MouseEvent): void {
+    console.log('set')
+    let currentCoordinates;
+    let targetColor;
+    let replaceColor;
+    if (this.toolConfig.colorValue && this.toolConfig.penSize && this.context !== null) {
+      switch(this.toolConfig.nameTool) {
+        case NameTools.Stroke: // add to schema line on draw
+          if(!this.isDrawing) return;
+          this.useTool(this.startCoordinates, this.endCoordinates, this.toolConfig.colorValue, this.toolConfig.penSize);
+          break;
+        case NameTools.Bucket: // fix work bucket for clear list
+          currentCoordinates = getCoordinates(this.canvas, e);
+          targetColor = this.context.getImageData(currentCoordinates.x, currentCoordinates.y, 1, 1).data.toString();
+          replaceColor = convertColor.hexToRgba(this.toolConfig.colorValue);
+          this.bucketPart(targetColor, replaceColor, currentCoordinates);
+          break;
+        case NameTools.Clear: //fix onClick because onMove draw px mb better to downClick
+          this.clearCanvas();
+      }
+    }
+    
+    this.isDrawing = false;
+  }
+  
+  public clearCanvas(): void {
+    if (this.context !== null) {
+      this.context.fillStyle = '#ffffff';
+      this.context.fillRect(0, 0, 32, 32);
+    }
+  }
+  
+  //fix method for bucket canvas
+  public bucketPart(targetColor: string, replaceColor: string, coors: Point) {  
+    if (targetColor === replaceColor) return;
+  
+    const replaceColorHex = convertColor.rgbaToHex(replaceColor);
+    
+    if(this.context !== null) {
+      this.context.fillStyle = replaceColorHex;
+      this.context.fillRect(coors.x, coors.y, 1, 1);
+      
+      const queue: Point[] = [];
+      queue.push(coors);
+
+      while (queue.length) {
+        const node = queue[0];
+        console.log(node)
+        queue.shift();
+    
+        const rightNode = {
+          x: node.x + 1,
+          y: node.y
+        };
+
+        const leftNode = {
+          x: node.x - 1,
+          y: node.y
+        };
+
+        const bottomNode = {
+          x: node.x,
+          y: node.y + 1
+        };
+
+        const topNode = {
+          x: node.x,
+          y: node.y - 1 
+        };
+    
+        if (this.context.getImageData(rightNode.x, rightNode.y, 1, 1).data.toString() === targetColor) {
+          this.context.fillRect(rightNode.x, rightNode.y, 1, 1);
+          queue.push(rightNode);
+        }
+        
+        if (this.context.getImageData(leftNode.x, leftNode.y, 1, 1).data.toString() === targetColor) {
+          this.context.fillRect(leftNode.x, leftNode.y, 1, 1);
+          queue.push(leftNode);
+        }
+        
+        if (this.context.getImageData(bottomNode.x, bottomNode.y, 1, 1).data.toString() === targetColor) {
+          this.context.fillRect(bottomNode.y, bottomNode.y, 1, 1);
+          queue.push(bottomNode);
+        }
+        
+        if (this.context.getImageData(topNode.x, topNode.y, 1, 1).data.toString() === targetColor) {
+          this.context.fillRect(topNode.x, topNode.y, 1, 1);
+          queue.push(topNode);
+        }
+      }
+    }
+  }
 }
