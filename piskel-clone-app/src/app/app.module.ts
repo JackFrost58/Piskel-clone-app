1265004665e6b7343055b96e192ce89c475a979d
Vladislav Zhgirov <zgv2324@gmail.com>
2023-03-06 20:01:07 +0300

    app: add new components

--- piskel-clone-app/src/app/app.module.ts
+++ piskel-clone-app/src/app/app.module.ts
@@ -2,7 +2,10 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AnimationContainerModule} from '@shared/animation-container/animation-container.module';
 import {CanvasContainerModule} from '@shared/canvas-container/canvas-container.module';
+import {FrameContainerModule} from '@shared/frame-container/frame-container.module';
+import {HeaderContainerModule} from '@shared/header-container/header-container.module';
 import {ToolsModule} from '@shared/tools-container/tools.module';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -17,7 +20,10 @@ import {AppComponent} from './app.component';
     ToolsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    CanvasContainerModule
+    HeaderContainerModule,
+    CanvasContainerModule,
+    AnimationContainerModule,
+    FrameContainerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
