1435d0b1d4baeaea45f77170958a552aa3a2a5d2
Vladislav Zhgirov <zgv2324@gmail.com>
2023-03-11 22:59:52 +0300

    del no need methods

--- piskel-clone-app/src/app/entities/helpers/content-frame.helper.ts
+++ piskel-clone-app/src/app/entities/helpers/content-frame.helper.ts
@@ -17,39 +17,5 @@ export class ContentCanvas {
   //     pasteImage(url, frames[index].children[1]);
   //     pasteImage(url, canvas);
   //   })
-  // }
-  
-  public static getContentFromFrames() {
-    const frameContainers = document.querySelectorAll('.frame-container');
-    // const imageFromFrames = Array.from(frameContainers).map(frame => 
-      // frame.firstElementChild.children[1].toDataURL());
-  
-    // return imageFromFrames;
-  }
-  
-  // public pasteImage(url, canvasEl) {
-  //   const canvas = canvasEl;
-  //   const size = localStorage.getItem('size');
-  //   const context = canvas.getContext('2d');
-  //   const img = new Image();
-  //   img.src = url;
-  
-  //   canvas.width = size;
-  //   canvas.height = size;
-  
-  //   img.onload = () => {
-  //     context.fillStyle = '#fff';
-  //     context.fillRect(0, 0, size, size);
-  //     context.imageSmoothingEnable = false;
-  //     context.drawImage(img, 0, 0);
-  //   }
-  // }
-  
-  // public pasteOnFrame(canvas) {   
-  //   const currentFrame = document.querySelector('.frame--active');
-  //   const currentCanvas = currentFrame.children[1];
-  
-  //   pasteImage(canvas.toDataURL(), currentCanvas);
-  // }
-  
+  // }  
 }
