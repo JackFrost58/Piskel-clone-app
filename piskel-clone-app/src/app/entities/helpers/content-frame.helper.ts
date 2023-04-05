3bc3f21d8a7be4a11e86c557015db9d14ef92fc3
Vladislav Zhgirov <zgv2324@gmail.com>
2023-04-05 22:43:37 +0300

    content-frame.helper: refactor method setCanvasContent

--- piskel-clone-app/src/app/entities/helpers/content-frame.helper.ts
+++ piskel-clone-app/src/app/entities/helpers/content-frame.helper.ts
@@ -1,10 +1,7 @@
-import {COLOR_WHITE} from "@constants/tools.constant";
-
 export class ContentCanvas {
   public static setCanvasContent(content: string, context: CanvasRenderingContext2D | null, sizeCanvas: number) {
     if (context !== null && context) {
-      context.fillStyle = COLOR_WHITE;
-      context.fillRect(0, 0, sizeCanvas, sizeCanvas);
+      context.clearRect(0, 0, sizeCanvas, sizeCanvas)
 
       if (content) {
         const image = new Image();
