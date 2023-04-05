2f42966bb8dfd4cf60b47ed393cdea4a45c46573
Vladislav Zhgirov <zgv2324@gmail.com>
2023-04-05 22:45:13 +0300

    canvas-container.component: fix method draw stroke

--- piskel-clone-app/src/app/components/canvas-container/canvas-container.component.ts
+++ piskel-clone-app/src/app/components/canvas-container/canvas-container.component.ts
@@ -1,8 +1,8 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {convertColor} from '@helpers/convert-colors.helper';
-import {getCoordinates} from '@helpers/coordinates.helper';
+import {getCoordinates} from './helper/coordinates.helper';
 import {ConfigTool} from '@interfaces/config-tool.interface';
-import {Point} from '@interfaces/coordinate.interface';
+import {Point, PointConfig} from '@interfaces/coordinate.interface';
 import {FrameContentService} from '@services/frame-content.service';
 import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 import {ContentCanvas} from '@helpers/content-frame.helper';
@@ -21,17 +21,19 @@ import {COLOR_WHITE} from '@constants/tools.constant';
 export class CanvasContainerComponent implements OnInit {
   @Input() toolConfig: ConfigTool;
 
-  public startCoordinates: Point;
-  public endCoordinates: Point;
   public context: CanvasRenderingContext2D | null;
 
+  public contentConfig: PointConfig[] = [];
+  public pointsStroke: PointConfig[] = []
+  public startCoordinates: Point = {x: 0, y: 0};
   public isDrawing = false;
+  public isLeftButtonDown = false;
   public sizeCanvas = 32;
 
   constructor(private frameContentService: FrameContentService, private sizeService: SizeService) {}
 
   ngOnInit(): void {
-    this.startCoordinates = {x: 0, y: 0};
+    // this.startCoordinates = {x: 0, y: 0};
     
     this.sizeService.size
     .pipe(untilDestroyed(this))
@@ -43,20 +45,6 @@ export class CanvasContainerComponent implements OnInit {
     
     this.context = this.canvas.nativeElement.getContext('2d');
 
-    // if (this.context !== null) {
-    //   for (let x = 0; x <= this.sizeCanvas; x += 1) {
-    //     this.context.moveTo(x, 0);
-    //     this.context.lineTo(x, this.sizeCanvas);
-    //     for (let y = 0; y <= this.sizeCanvas; y += 1) {
-    //       this.context.moveTo(0, y);
-    //       this.context.lineTo(this.sizeCanvas, y);
-    //     }
-    //   }
-
-    //   this.context.strokeStyle = 'rgb(20,20,20)'
-    //   this.context.stroke();
-    // }
-
     this.frameContentService.canvasContent
     .pipe(untilDestroyed(this))
     .subscribe((canvasContent: string) => {
@@ -67,82 +55,131 @@ export class CanvasContainerComponent implements OnInit {
   @ViewChild('canvasElement', {static: true})
   canvas: ElementRef<HTMLCanvasElement>;
 
-  public useTool(startCoord: Point, currentCoord: Point, tool: ConfigTool): void {
-    if (this.context !== null && tool) {    
-      const deltaX = Math.abs(currentCoord.x - startCoord.x);
-      const deltaY = Math.abs(currentCoord.y - startCoord.y);
-      const signX = (startCoord.x < currentCoord.x) ? 1 : -1;
-      const signY = (startCoord.y < currentCoord.y) ? 1 : -1;
-      let err = deltaX - deltaY;
-    
-      while(true) {
-        console.log(currentCoord)
-        ToolCheckHelper.isToolWithColor(tool.nameTool) ? this.context.fillStyle = COLOR_WHITE : this.context.fillStyle = tool.colorValue;
-       
-        this.context.fillRect(tool.penSize*startCoord.x, tool.penSize*startCoord.y, tool.penSize, tool.penSize) 
-
-        if ((startCoord.x === Number(currentCoord.x)) && (startCoord.y === Number(currentCoord.y))) break;
-        
-        //out of while
-        const e2 = 2 * err;
-        if (e2 > -deltaY) { 
-          err -= deltaY; 
-          startCoord.x  += signX; 
-        }
-        if (e2 < deltaX) { 
-          err += deltaX; 
-          startCoord.y  += signY; 
-        }
-      }
+  public drawPoint(pointCoordinate: Point, tool: ConfigTool): void {
+    if (this.context !== null && tool) {      
+      ToolCheckHelper.isToolWithColor(tool.nameTool) ? this.context.fillStyle = COLOR_WHITE : this.context.fillStyle = tool.colorValue;
+      this.context.fillRect(tool.penSize*pointCoordinate.x, tool.penSize*pointCoordinate.y, tool.penSize, tool.penSize);
     }
   }
 
-  public canvasMouseDownHandler(e: MouseEvent): void {
-    console.log(e)
+  public canvasMouseDownHandler(event: MouseEvent): void {
+    this.toggleMouseButton();
+
     switch (this.toolConfig.nameTool) {
       case NameTools.Eraser:
       case NameTools.Pen:
       case NameTools.Stroke:
         this.isDrawing = true;
-        this.startCoordinates = getCoordinates(this.canvas, e, this.sizeCanvas, this.toolConfig.penSize);
-        this.useTool(this.startCoordinates, this.startCoordinates, this.toolConfig);
+        this.startCoordinates = getCoordinates(this.canvas, event, this.sizeCanvas, this.toolConfig.penSize);
+
+        this.drawPoint(this.startCoordinates, this.toolConfig);
+        this.addPointCanvas([{...this.startCoordinates, tool: {...this.toolConfig}}]);
         break;
       case NameTools.Clear:
         this.clearCanvas();
         break;
     }
+
+    // this.savePrevStateCanvas();
   }
 
-  public canvasMouseMoveHandler(e: MouseEvent): void {
-    let currentCoordinates;
-    if (this.toolConfig.colorValue && this.toolConfig.penSize) {
+  public canvasMouseMoveHandler(event: MouseEvent): void {
+    let currentCoordinates: Point;
+   
+    if (this.toolConfig.colorValue && this.toolConfig.penSize && this.isLeftButtonDown) {
       switch (this.toolConfig.nameTool) {
         case NameTools.Eraser:
         case NameTools.Pen: 
-          currentCoordinates = getCoordinates(this.canvas, e, this.sizeCanvas, this.toolConfig.penSize);
+          currentCoordinates = getCoordinates(this.canvas, event, this.sizeCanvas, this.toolConfig.penSize);
           if(!this.isDrawing) return;
-          this.useTool(currentCoordinates, currentCoordinates, this.toolConfig);
+          this.drawPoint(currentCoordinates, this.toolConfig);
+          this.addPointCanvas([{...currentCoordinates, tool: {...this.toolConfig}}]);
+
           break;
         case NameTools.Stroke:
-          this.endCoordinates = getCoordinates(this.canvas, e, this.sizeCanvas, this.toolConfig.penSize);
+          currentCoordinates = getCoordinates(this.canvas, event, this.sizeCanvas, this.toolConfig.penSize);
+
+          this.brezenhaimLine(this.startCoordinates, currentCoordinates);
           break;
       }
     }
   }
 
-  public canvasMouseUpHandler(e: MouseEvent): void {
+  public brezenhaimLine(pointStart: Point, pointEnd: Point): void {
+    this.pointsStroke = []
+
+   this.drawPreviousContent();
+
+    const clonePointStart = {...pointStart};
+
+    const deltaX = Math.abs(pointEnd.x - clonePointStart.x);
+    const deltaY = Math.abs(pointEnd.y - clonePointStart.y);
+    const signX = (clonePointStart.x < pointEnd.x) ? 1 : -1;
+    const signY = (clonePointStart.y < pointEnd.y) ? 1 : -1;
+    let err = deltaX - deltaY;
+
+    while(true) {
+      this.pointsStroke.push({...clonePointStart, tool: {...this.toolConfig}});
+      this.drawPoint(clonePointStart, this.toolConfig);
+
+      if ((clonePointStart.x === pointEnd.x) && (clonePointStart.y === pointEnd.y)) break;
+
+      //out of while
+      const e2 = 2 * err;
+      
+      if (e2 > -deltaY) { 
+        err -= deltaY; 
+        clonePointStart.x  += signX; 
+      }
+      
+      if (e2 < deltaX) { 
+        err += deltaX; 
+        clonePointStart.y  += signY; 
+      }
+    }
+  }
+
+  public drawSquare(): void {
+    // if(!this.isDrawing) return;
+
+    // this.endCoordinates = getCoordinates(this.canvas, event, this.sizeCanvas, this.toolConfig.penSize);
+    if (this.context !== null && this.startCoordinates.x) {
+      this.context.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height)
+      // this.rects.forEach((rect: any) => {
+      //   if (this.context !== null) {
+      //     this.context.beginPath();
+      //     // this.useTool(rect.x, rect.y, rect.width)
+      //     this.context.rect(rect.x, rect.y, rect.width, rect.height);
+      //     this.context.strokeStyle = this.toolConfig.colorValue;
+      //     this.context.stroke();
+      //   }
+      // }); 
+
+      // this.context.beginPath();
+      // this.context.rect(this.startCoordinates.x, this.startCoordinates.y, this.endCoordinates.x - this.startCoordinates.x, this.endCoordinates.y - this.startCoordinates.y);
+      // this.context.strokeStyle = 'red';
+      // this.context.stroke();
+      // this.context.beginPath();
+      // this.context.fill();
+      // this.context.beginPath();
+      // this.context.fill();
+    }
+  }
+
+  public canvasMouseUpHandler(event: MouseEvent): void {
     let currentCoordinates;
     let targetColor;
     let replaceColor;
 
+    this.toggleMouseButton();
+
     if (this.context !== null) {
       switch(this.toolConfig.nameTool) {
-        case NameTools.Stroke: // add to schema line on draw
-          if(!this.isDrawing) return;
-          this.useTool(this.startCoordinates, this.endCoordinates, this.toolConfig);
+        case NameTools.Stroke:
+          this.addPointCanvas(this.pointsStroke)
           break;
         case NameTools.Bucket: // fix work bucket for clear list
-          currentCoordinates = getCoordinates(this.canvas, e, this.sizeCanvas, this.toolConfig.penSize);
+          currentCoordinates = getCoordinates(this.canvas, event, this.sizeCanvas, this.toolConfig.penSize);
           targetColor = this.context.getImageData(currentCoordinates.x, currentCoordinates.y, 1, 1).data.toString();
           replaceColor = convertColor.hexToRgba(this.toolConfig.colorValue);
           this.bucketPart(targetColor, replaceColor, currentCoordinates);
@@ -152,13 +189,6 @@ export class CanvasContainerComponent implements OnInit {
     
     this.isDrawing = false;
     this.frameContentService.setContentFrame(this.canvas.nativeElement.toDataURL());
-  }
-  
-  public clearCanvas(): void {
-    if (this.context !== null) {
-      this.context.fillStyle = COLOR_WHITE;
-      this.context.fillRect(0, 0, this.sizeCanvas, this.sizeCanvas);
-    }
   }
   
   //fix method for bucket canvas
@@ -176,7 +206,7 @@ export class CanvasContainerComponent implements OnInit {
 
       while (queue.length) {
         const node = queue[0];
-        console.log(node)
+        // console.log(node)
         queue.shift();
     
         const rightNode = {
@@ -220,5 +250,32 @@ export class CanvasContainerComponent implements OnInit {
         }
       }
     }
+  }
+
+  public clearCanvas(): void {
+    if (this.context !== null) {
+      this.context.fillStyle = COLOR_WHITE;
+      this.context.fillRect(0, 0, this.sizeCanvas, this.sizeCanvas);
+      // this.rects = [];
+    }
+  }
+
+  private toggleMouseButton(): void {
+    this.isLeftButtonDown = !this.isLeftButtonDown;
+  }
+
+  private addPointCanvas(newPoint: PointConfig[]): void {
+    const content = [...newPoint, ...this.contentConfig];
+    this.contentConfig = content.filter((point, index) => content.findIndex((s) => point.x === s.x && point.y === s.y) === index);
+  }
+
+  private drawPreviousContent(): void {
+    if (this.context !== null) {
+      this.context.clearRect(0, 0, this.sizeCanvas, this.sizeCanvas);
+    }
+    
+    this.contentConfig.forEach((item) => {
+      this.drawPoint({x: item.x, y: item.y}, item.tool)
+    })
   }
 }
